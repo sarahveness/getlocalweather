754c753d9148c26798e6b53ba4030acfd4798a01
Sarah Veness <sarahhveness@gmail.com>
2017-07-25 16:05:17 -0700

    pulling in API data, temp image placeholder applied, can change from celcius to fareinheit

--- js/index.js
+++ js/index.js
@@ -1,33 +1,42 @@
-require('dotenv').config()
-
 $(document).ready(function() {
+  var long;
+  var lat;
+  var weatherType;
+  var city;
+  var cTemp;
+  var fTemp;
+  var tempChange = true;
 
-  var x = document.getElementById("demo");
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
 
-  function getLocation() {
-      if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(showPosition);
-      } else {
-          x.innerHTML = "Geolocation is not supported by this browser.";
-      }
-  }
-
-  function showPosition(position) {
-    var long = position.coords.longitude;
-    var lat = position.coords.latitude;
-
-    function getWeather() {
-      var url= `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}`;
-
+      var url = `//api.openweathermap.org/data/2.5/weather?&lat=${lat}&lon=${long}&APPID=APIKEY`;
       $.getJSON(url, function(data) {
-        console.log(data)
+        var weatherType = data.weather[0].description;
+        var city = data.name;
+        var kTemp = data.main.temp;
+        var humidity = data.main.humidity;
+
+        cTemp = Math.round(kTemp - 273);
+        fTemp = Math.round(cTemp * 1.8 + 32);
+
+        $("#city").html(city);
+        $("#temperature").html(`${cTemp} °C`);
+
+        $("#temperature").on('click', function() {
+          if(tempChange === false) {
+            $("#temperature").html(`${cTemp} °C`);
+            tempChange = true;
+          } else {
+            $("#temperature").html(`${fTemp} °F`);
+            tempChange = false;
+          };
+        })
+
+
       });
-    };
+    })
   }
-
-
-
-
-
-window.onload = getLocation();
 });
